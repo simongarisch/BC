bc99fb497e54b3be35e951392fca50e10b85a1d1
simongarisch <31283453+simongarisch@users.noreply.github.com>
2024-11-30 07:14:20 +0000

    adding bools

--- vars.c
+++ vars.c
@@ -1,11 +1,20 @@
 // gcc -Wall -Wextra -std=c2x -pedantic vars.c -o vars.out
 // ./vars.out
 #include <stdio.h>
+#include <stdbool.h> // header was introduced in C99
 
 int main(void) {
+    // int, float and char...
     int i = 2;
     float f = 3.14;
     char *s = "hello, world";
 
     printf("%s i=%d and f=%.2f\n", s, i, f);
+
+    // bools
+    bool x = true;
+
+    if (x) {
+        printf("x is true!\n");
+    }
 }
